acefa883207ac0e8442b65cf5badf5fa405c7de8
wh1012 <63694003+kangwoohyun999@users.noreply.github.com>
2025-10-10 21:29:23 +0900

    Add files via upload

---
+++ W6/W6_work01.py
@@ -0,0 +1,43 @@
+print("*********** Command ************")
+print("+<c> : AddQ, - : DeleteQ,\nS : Show, Q : Quit")
+print("********************************")
+
+# 큐 초기화
+queue = []
+MAX_SIZE = 8
+
+def queue_func(command):
+    # AddQ
+    if command[0] == "+":
+        if len(queue) > MAX_SIZE:
+            print("Queue is full !!!")
+            return True
+        queue.append(command[1])
+    
+    # DeleteQ
+    elif command == "-":
+        if len(queue) == 0:
+            print("Queue is empty !!!")
+            return True
+        print(queue[0])
+        queue.pop(0)
+    
+    # Show
+    elif command == "S" or command == "s":
+        for item in queue:
+            print(item, end=" ")
+        print()
+    
+    # Quit
+    elif command == "Q" or command == "q":
+        print("Quit")
+        return False
+    
+    return True
+
+
+# 실행 루프
+while True:
+    command = input("Command> ")
+    if not queue_func(command):
+        break
